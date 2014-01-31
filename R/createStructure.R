f54919588775f03951f5024e2e63e74b1470dfde
fdetsch <flowla88@gmail.com>
2014-01-31 12:36:49 +0100

    Update: implemented 'Roxygenize' functionality.

--- R/createStructure.R
+++ R/createStructure.R
@@ -1,10 +1,13 @@
+#' Create folder structure required by MSEF.
+#' 
+#' @export createStructure
 createStructure <- function(dsn = ".", 
                             ...) {
 
   path.in <- "input"
   path.out <- "output"
   
-  for (i in c(path.func, path.config, path.in)) {
+  for (i in c(path.in, path.out)) {
     
     if (!file.exists(paste(dsn, i, sep = "/"))) 
       dir.create(paste(dsn, i, sep = "/"))
